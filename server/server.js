f3869808e4e5826b4fb814e889176303d3a1afef
Rens Pennings <renspennings@gmail.com>
2021-05-02 16:20:06 +0200

    updates

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+
 const compression = require("compression");
 const path = require("path");
+
 const cookieSession = require("cookie-session");
+
 const csurf = require("csurf");
 const multer = require("multer");
 const uidSafe = require("uid-safe");
